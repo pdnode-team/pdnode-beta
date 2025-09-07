ca7e88177c48f87b7c44299f7ddec6309af2e741
XxdMkbMark <103048057+XxdMkbMark@users.noreply.github.com>
2025-09-07 18:18:24 +0800

    bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 "use client"; //DO NOT TOUCH THIS OR THE WHOLE PROJECT WILL BE MESSED UP
 
 import * as React from "react";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <React.Fragment>
+      <Head>
+          <link rel="icon" sizes={"128x128"} href="/public/favicon.ico" />
+      </Head>
       <title>PdNode Beta</title>
       <div className={"navi-bar"}>
-          <a>1</a>
+        <Button><Image alt={"PD Node Beta Icon"} src={"next.svg"} width={50} height={50}/></Button>
       </div>
     </React.Fragment>
   );
