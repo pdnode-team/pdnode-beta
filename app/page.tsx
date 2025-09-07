539c8df4b946daea0f9eb16a2078f62c09db1189
XxdMkbMark <103048057+XxdMkbMark@users.noreply.github.com>
2025-09-07 10:47:06 +0800

    bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+"use client"; //DO NOT TOUCH THIS OR THE WHOLE PROJECT WILL BE MESSED UP
+
+import * as React from "react";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <React.Fragment>
       <title>PdNode Beta</title>
+      <div>
+          <a>1</a>
+      </div>
+    </React.Fragment>
   );
 }
