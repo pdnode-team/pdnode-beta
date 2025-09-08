1288df8b32cc5586472b0765d35802b18fd7c68a
XxdMkbMark <103048057+XxdMkbMark@users.noreply.github.com>
2025-09-08 19:16:13 +0800

    bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -24,16 +24,18 @@ export default function Home() {
         <link rel="icon" sizes={"128x128"} href="/favicon.ico" />
       </Head>
       <title>PdNode Beta - All beta projects are in here</title>
-        <Stack direction={"row"} spacing={5} className={"navi-bar"}>
+        <Stack direction={"row"} spacing={4} className={"navi-bar"}>
           <div>
             <Button href="/" sx={{background:"rgb(142 77 47)", marginLeft:"13px", marginTop:"13px", borderRadius:"16px", height:"63px", width:"200px"}}>
               <Image alt={"PD Node Beta Icon"} src={"/pdnode-beta-icon-dark.png"} width={160} height={160}/>
             </Button>
           </div>
           <div>
-            <Stack direction="row" spacing={2} sx={{marginTop:"13px", justifyItems:"center"}}>
-              <Button href="/" sx={{background:"rgb(119 87 74)", borderRadius:"100px", height:"35px", width:"45px"}}><HomeIcon sx={{color:"#fefbff"}}/></Button>
-              <Typography></Typography>
+            <Stack direction="row" spacing={2} sx={{marginTop:"23px", justifyItems:"center"}}>
+              <Stack sx={{alignItems:"center"}}>
+                <Button href="/" sx={{background:"rgb(119 87 74)", borderRadius:"100px", height:"32px", width:"45px"}}><HomeIcon sx={{color:"#fefbff"}}/></Button>
+                <Typography sx={{color:"rgb(35 26 22)", fontSize:"0.85rem", fontWeight:"500"}}>Home</Typography>
+              </Stack>
             </Stack>
           </div>
         </Stack>
