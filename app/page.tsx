7fe3494f87a8bf853ebdb2f65fab5a85bf644c1a
XxdMkbMark <103048057+XxdMkbMark@users.noreply.github.com>
2025-09-08 18:57:34 +0800

    added button

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 "use client"; //DO NOT TOUCH THIS OR THE WHOLE PROJECT WILL BE MESSED UP
 
+import "./globals-light.css";
 import * as React from "react";
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 import HomeIcon from '@mui/icons-material/Home';
 
 import Image from "next/image";
 import Head from "next/head";
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 export default function Home() {
   return (
@@ -16,16 +24,19 @@ export default function Home() {
         <link rel="icon" sizes={"128x128"} href="/favicon.ico" />
       </Head>
       <title>PdNode Beta - All beta projects are in here</title>
-      <div className={"navi-bar"}>
-        <Stack>
-          <Button href="/" sx={{background:"#a4c7d3", marginLeft:"13px", marginTop:"13px", borderRadius:"16px", height:"63px", width:"200px"}}>
-            <Image alt={"PD Node Beta Icon"} src={"/pdnode-beta-icon-light.png"} width={160} height={160}/>
-          </Button>
-          <Stack direction="row" spacing={2}>
-            <Button href="/"><HomeIcon/></Button>
-          </Stack>
+        <Stack direction={"row"} spacing={5} className={"navi-bar"}>
+          <div>
+            <Button href="/" sx={{background:"rgb(142 77 47)", marginLeft:"13px", marginTop:"13px", borderRadius:"16px", height:"63px", width:"200px"}}>
+              <Image alt={"PD Node Beta Icon"} src={"/pdnode-beta-icon-dark.png"} width={160} height={160}/>
+            </Button>
+          </div>
+          <div>
+            <Stack direction="row" spacing={2} sx={{marginTop:"13px", justifyItems:"center"}}>
+              <Button href="/" sx={{background:"rgb(119 87 74)", borderRadius:"100px", height:"35px", width:"45px"}}><HomeIcon sx={{color:"#fefbff"}}/></Button>
+              <Typography></Typography>
+            </Stack>
+          </div>
         </Stack>
-      </div>
     </React.Fragment>
   );
 }
