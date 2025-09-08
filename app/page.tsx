99cb3ec31c5bcfc32e856eeea379987ddd4fc00b
XxdMkbMark <103048057+XxdMkbMark@users.noreply.github.com>
2025-09-08 18:05:21 +0800

    new button and bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 import * as React from "react";
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+
+import HomeIcon from '@mui/icons-material/Home';
+
 import Image from "next/image";
 import Head from "next/head";
 
@@ -10,13 +13,18 @@ export default function Home() {
   return (
     <React.Fragment>
       <Head>
-        <link rel="icon" sizes={"128x128"} href="/public/favicon.ico" />
+        <link rel="icon" sizes={"128x128"} href="/favicon.ico" />
       </Head>
       <title>PdNode Beta - All beta projects are in here</title>
       <div className={"navi-bar"}>
-        <Button href={"/"} sx={{background:"#a4c7d3", marginLeft:"13px", marginTop:"13px", borderRadius:"16px", height:"63px", width:"200px"}}>
-          <Image alt={"PD Node Beta Icon"} src={"/pdnode-beta-icon-light.png"} width={160} height={160}/>
-        </Button>
+        <Stack>
+          <Button href="/" sx={{background:"#a4c7d3", marginLeft:"13px", marginTop:"13px", borderRadius:"16px", height:"63px", width:"200px"}}>
+            <Image alt={"PD Node Beta Icon"} src={"/pdnode-beta-icon-light.png"} width={160} height={160}/>
+          </Button>
+          <Stack direction="row" spacing={2}>
+            <Button href="/"><HomeIcon/></Button>
+          </Stack>
+        </Stack>
       </div>
     </React.Fragment>
   );
