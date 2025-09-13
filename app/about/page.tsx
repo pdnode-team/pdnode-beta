9486e93e064e48cf4405f2d752931c08665e18a6
XxdMkbMark <103048057+XxdMkbMark@users.noreply.github.com>
2025-09-13 15:55:06 +0800

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -101,6 +101,10 @@ export default function Home() {
           </Stack>
         </Stack>
       </div>
+      <div>
+        <Typography variant={"h2"}>About Us</Typography>
+        <Typography>Pdnode is a group of people with the same interests. Founded on 2024-07-10.</Typography>
+      </div>
       <footer style={{position:"fixed", bottom:"10px", width:"100%", display:"flex", alignItems:"center", justifyContent:"center"}}>
         <Typography sx={{fontSize:"0.88rem", color:"gray"}}>v57 | © 2025 PdNode. All Rights Reserved. · <Link href="https://forum.pdnode.com/d/5-pdnode-terms-of-service">Terms</Link> · <Link href="https://forum.pdnode.com/d/5-pdnode-terms-of-service">Privacy</Link></Typography>
       </footer>
